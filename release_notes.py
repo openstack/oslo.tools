openstack-archive/oslo-incubator

Adjust release note template whitespace
Without a noteable changes file the release notes included extra blank
lines in a few places. Adjust the whitespace treatment to fix that, and
to indent URLs in the output.

Change-Id: Ib79a3fb237f2fab07cc9d30be4cc89b79957c7e5

tools/release_notes.py
@@ -33,11 +33,11 @@ The Oslo team is pleased to announce the release of:
 
 For more details, please see the git log history below and:
 
-{{ milestone_url }}
+    {{ milestone_url }}
 
 Please report issues through launchpad:
 
-{{ bug_url }}
+    {{ bug_url }}
 """
 
 # This will just be replaced with template values (no wrapping applied).
@@ -46,8 +46,7 @@ Noteable changes
 ----------------
 
 {{ noteables }}
-{% endif -%}
-
+{% endif %}
 {{ change_header }}
 
 {% for change in changes -%}
@@ -59,12 +58,14 @@ Diffstat (except docs and test files)
 {% for change in diff_stats -%}
 {{ change }}
 {% endfor %}
+{% if requirement_changes %}
 Requirements updates
 --------------------
 
 {% for change in requirement_changes -%}
 {{ change }}
 {% endfor %}
+{% endif %}
 """
 
 
@@ -166,7 +167,7 @@ def main():
     noteables = ''
     if args.noteable_changes:
         with open(args.noteable_changes, 'r') as fh:
-            noteables = fh.read()
+            noteables = fh.read().rstrip()
 
     lp_url = bug_url.replace("bugs.", "").rstrip("/")
     milestone_url = lp_url + "/+milestone/%s" % args.end_revision

openstack/oslo.tools

release_notes.py
@@ -33,11 +33,11 @@ The Oslo team is pleased to announce the release of:
 
 For more details, please see the git log history below and:
 
-{{ milestone_url }}
+    {{ milestone_url }}
 
 Please report issues through launchpad:
 
-{{ bug_url }}
+    {{ bug_url }}
 """
 
 # This will just be replaced with template values (no wrapping applied).
@@ -46,8 +46,7 @@ Notable changes
 ----------------
 
 {{ notables }}
-{% endif -%}
-
+{% endif %}
 {{ change_header }}{% if skip_requirement_merges %}
 
 NOTE: Skipping requirement commits...
@@ -62,12 +61,14 @@ Diffstat (except docs and test files)
 {% for change in diff_stats -%}
 {{ change }}
 {% endfor %}
+{% if requirement_changes %}
 Requirements updates
 --------------------
 
 {% for change in requirement_changes -%}
 {{ change }}
 {% endfor %}
+{% endif %}
 """
 
 CHANGES_ONLY_TPL = """{{ change_header }}
@@ -199,7 +200,7 @@ def main():
     notables = ''
     if args.notable_changes:
         with open(args.notable_changes, 'r') as fh:
-            notables = fh.read()
+            notables = fh.read().rstrip()
 
     lp_url = bug_url.replace("bugs.", "").rstrip("/")
     milestone_url = lp_url + "/+milestone/%s" % args.end_revision