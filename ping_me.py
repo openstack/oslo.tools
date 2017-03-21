c4dfe5dad33c6df3038dae145e6c5f2184bda880
Rushil Chugh <rushil.chugh@gmail.com>
2017-03-20 21:25:56 -0400

    Ping crushil instead of rloo
    crushil is the ironic liaison for oslo now.
    
    Change-Id: Ic1b6e74db7194fa51c96b8f1c09bc08ba647ae2f

--- ping_me.py
+++ ping_me.py
@@ -22,7 +22,7 @@ NOTIFY_PEPS = [
     'dansmith', 'dhellmann', 'dims', 'dougwig', 'e0ne', 'flaper87',
     'garyk', 'haypo', 'electrocucaracha', 'jd__', 'jecarey',
     'johnsom', 'jungleboyj', 'kgiusti', 'kragniz', 'lhx_',
-    'lifeless', 'lxsli', 'ozamiatin', 'redrobot', 'rloo',
+    'lifeless', 'lxsli', 'ozamiatin', 'redrobot', 'crushil',
     'rpodolyaka', 'spamaps', 'sergmelikyan', 'sreshetnyak',
     'sileht', 'sreshetnyak', 'stevemar', 'therve', 'thinrichs',
     'toabctl', 'viktors', 'zhiyan', 'zzzeek', 'gcb',
